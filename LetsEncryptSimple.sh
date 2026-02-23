7545274fa00f2843cf47a6951e8c306ab6b2dca6
Stevolus <25365857+Stevolus@users.noreply.github.com>
2026-02-23 10:06:13 +0200

    Create LetsEncryptSimple.sh

---
+++ LetsEncryptSimple.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Make sure the package 'certbot' is installed"
+echo ""
+echo "Cloudlinux/Centos yum -y install certbot"
+echo "Ubuntu/Debian apt -y install certbot"
+echo ""
+read "Enter the domain name you would like to get a certificate for: " domain
+
+sudo certbot certonly --standalone -d $domain
+
+echo "Please copy the below two lines to your apache or nginx server config file"
+echo "NB: Make sure that any existing entries are commented out or removed."
+
+ssl_certificate /etc/letsencrypt/live/$domain/cert.pem
+ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem
