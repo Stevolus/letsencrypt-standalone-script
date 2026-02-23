fe10e2287dba54a02102ab86bc63894520077269
Stevolus <25365857+Stevolus@users.noreply.github.com>
2026-02-23 11:18:15 +0200

    Update LetsEncryptSimple.sh

--- LetsEncryptSimple.sh
+++ LetsEncryptSimple.sh
@@ -7,10 +7,11 @@ echo "Ubuntu/Debian apt -y install certbot"
 echo ""
 read "Enter the domain name you would like to get a certificate for: " domain
 
+echo "Creating ssl certificate for $domain domain..."
+sleep 3
 sudo certbot certonly --standalone -d $domain
 
-echo "Creating ssl certificate for $domain domain..."
-sleep 2
+
 echo "Please copy the below two lines to your apache or nginx server config file"
 echo "NB: Make sure that any existing entries are commented out or removed."
 
